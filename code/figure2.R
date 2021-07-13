fb9b6c8e3cdd384da977c4fe06c87aec9caccc9f
Jarad Niemi <niemi@iastate.edu>
2021-07-13 16:30:52 -0500

    Add function to calculation emissions

---
+++ code/figure2.R
@@ -0,0 +1,15 @@
+library("tidyverse")
+
+source("emissions.R")
+
+d = data.frame(N = seq(0,240, by=40)) %>% 
+  mutate(emissions = emissions(N),
+         hw = emissions_hw(N),
+         lower = emissions - 2 * hw,
+         upper = emissions + 2 * hw)
+
+ggplot(d, aes(N)) + 
+  geom_line(aes(y = emissions), linetype = "dotted") + 
+  geom_line(aes(y = lower)) + 
+  geom_line(aes(y = upper)) +
+  theme_bw()
