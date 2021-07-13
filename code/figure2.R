46846e3aa0b4aecb327a42a7747870183651022d
Jarad Niemi <niemi@iastate.edu>
2021-07-13 16:36:39 -0500

    Use exact z critical values

--- code/figure2.R
+++ code/figure2.R
@@ -5,8 +5,8 @@ source("emissions.R")
 d = data.frame(N = seq(0,240, by=40)) %>% 
   mutate(emissions = emissions(N),
          hw = emissions_hw(N),
-         lower = emissions - 2 * hw,
-         upper = emissions + 2 * hw)
+         lower = emissions - qnorm(.975) * hw,
+         upper = emissions + qnorm(.975) * hw)
 
 ggplot(d, aes(N)) + 
   geom_line(aes(y = emissions), linetype = "dotted") + 
