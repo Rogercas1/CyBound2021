9557a72c443a829031be95610de46492f9b03aba
Roger Castillo <rogercas@iastate.edu>
2021-07-14 10:52:00 -0500

    Fix confidence interval calculation

--- code/figure2.R
+++ code/figure2.R
@@ -5,8 +5,8 @@ source("emissions.R")
 d = data.frame(N = seq(0,240, by=40)) %>% 
   mutate(emissions = emissions(N),
          hw = emissions_hw(N),
-         lower = emissions - qnorm(.975) * hw,
-         upper = emissions + qnorm(.975) * hw)
+         lower = emissions - hw,
+         upper = emissions + hw)
 
 ggplot(d, aes(N)) + 
   geom_line(aes(y = emissions), linetype = "dotted") + 
