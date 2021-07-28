a2f980da18ed03ec9a2d4759d2daa131f2e322e9
nasyring <nasyrin@gmail.com>
2021-07-28 08:37:54 -0500

    Update emissions.R
    Add runoff function from Lawlor et al. Fig 4

--- code/emissions.R
+++ code/emissions.R
@@ -9,3 +9,7 @@ emissions_hw <- function(N) {
   # 2.04*sqrt(exp(.0135*N*(4090-34*N+.075*N^2))) # typo in Table 1??
   2.04*sqrt(exp(.0135*N)*(4090-34*N+.075*N^2))
 }
+
+runoff <- function(N) {
+  5.72+1.33*exp(.0104*N)
+}
